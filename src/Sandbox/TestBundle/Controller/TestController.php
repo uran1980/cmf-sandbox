c449a27e7c19873edd08a65c0c9559bcb7758542
Alain Horner <alain.horner@liip.ch>
2012-04-25 19:24:23 +0200

    fe-editing for magnolia content

--- src/Sandbox/TestBundle/Controller/TestController.php
+++ src/Sandbox/TestBundle/Controller/TestController.php
@@ -11,4 +11,36 @@ class TestController extends Controller
         return $this->render('SandboxTestBundle:Test:index.html.twig', array('title'=>'Normal Symfony Route'));
     }
 
+    public function magnoliaAction ()
+    {
+        $path = '/demo-project/about/subsection-articles/large-article/content';
+
+        $session = $this->get('doctrine_phpcr')->getConnection('website');
+        $node = $session->getNode($path);
+        $content = $node->getPropertyValue('phpcr');
+
+        return $this->render('SandboxTestBundle:Test:magnolia.html.twig', array(
+            'title'=>'Normal Symfony Route',
+            'page' => array(
+                'tags' => array("hulla", "holla"),
+                'path' => $path,
+                'title' => 'mytitle',
+                'content' => $content
+            )
+        ));
+    }
+
+    public function magnoliaWriteAction ($contentPath)
+    {
+        $params = $this->getRequest()->request->all();
+
+        $session = $this->get('doctrine_phpcr')->getConnection('website');
+        $node = $session->getNode('/' . $contentPath);
+
+        $node->setProperty('phpcr', $params['<http://rdfs.org/sioc/ns#content>']);
+        $session->save();
+
+        return new \Symfony\Component\HttpFoundation\Response();
+    }
+
 }
