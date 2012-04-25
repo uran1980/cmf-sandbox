b768a85aa96e643bf053138fff707c375d833e97
lsmith77 <smith@pooteeweet.org>
2012-04-25 23:14:15 +0200

    added a simple example reading some content out of Magnolia CMS

--- src/Sandbox/TestBundle/Controller/TestController.php
+++ src/Sandbox/TestBundle/Controller/TestController.php
@@ -11,36 +11,40 @@ class TestController extends Controller
         return $this->render('SandboxTestBundle:Test:index.html.twig', array('title'=>'Normal Symfony Route'));
     }
 
-    public function magnoliaAction ()
+    public function magnoliaAction($article)
     {
-        $path = '/demo-project/about/subsection-articles/large-article/content';
+        $website = $this->get('doctrine_phpcr')->getConnection('website');
 
-        $session = $this->get('doctrine_phpcr')->getConnection('website');
-        $node = $session->getNode($path);
-        $content = $node->getPropertyValue('phpcr');
+        $node = $website->getNode("/demo-project/$article");
 
-        return $this->render('SandboxTestBundle:Test:magnolia.html.twig', array(
-            'title'=>'Normal Symfony Route',
-            'page' => array(
-                'tags' => array("hulla", "holla"),
-                'path' => $path,
-                'title' => 'mytitle',
-                'content' => $content
-            )
-        ));
+        $dms = $this->get('doctrine_phpcr')->getConnection('dms');
+
+        $template = $node->getNode('MetaData')->getPropertyValue('mgnl:template');
+
+        switch ($template) {
+            case "standard-templating-kit:pages/stkSection":
+                break;
+            case "standard-templating-kit:pages/stkLargeArticle":
+            case "standard-templating-kit:pages/stkArticle":
+                $subnode = $node->getNode("content");
+                $subnode->setProperty('phpcr', 'was here!!');
+                $website->save();
+                $properties = $subnode->getNode('00')->getPropertiesValues();
+
+                $imgNode = $dms->getNodeByIdentifier($properties['imageDmsUUID'])->getNode('document');
+                $img = $imgNode->getPropertiesValues();
+                $img['jcr:data'] = base64_encode(stream_get_contents($imgNode->getProperty('jcr:data')->getBinary()));
+
+                return $this->render('SandboxTestBundle:Test:magnolia.html.twig', array(
+                        'node' => $node,
+                        'article' => $node->getPropertiesValues(),
+                        'content' => $properties,
+                        'title' => 'foo',
+                        'img' => $img,
+                    )
+                );
+        }
+
+        throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException('Could not map content');
     }
-
-    public function magnoliaWriteAction ($contentPath)
-    {
-        $params = $this->getRequest()->request->all();
-
-        $session = $this->get('doctrine_phpcr')->getConnection('website');
-        $node = $session->getNode('/' . $contentPath);
-
-        $node->setProperty('phpcr', $params['<http://rdfs.org/sioc/ns#content>']);
-        $session->save();
-
-        return new \Symfony\Component\HttpFoundation\Response();
-    }
-
 }
